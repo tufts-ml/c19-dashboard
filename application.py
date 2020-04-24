dad88a24e86056689c5da8c55cf59cc9257883b5
amosca01 <amosca01@eecs.tufts.edu>
2020-04-24 08:56:52 -0400

    showing multiple plots

--- application.py
+++ application.py
@@ -11,6 +11,9 @@ from components.charts import chart_panel
 from components.controls import control_panel
 from components.header import header_panel
 
+import pandas as pd 
+from collections.abc import Iterable
+
 dash_app = dash.Dash(__name__)
 dash_app.config.suppress_callback_exceptions = True
 application = dash_app.server
@@ -22,125 +25,182 @@ dash_app.layout = html.Div(
     className='container', children=[
         header_panel(),
         control_panel(plot_options, len(plot_dicts)),
-        chart_panel(),   
-    ]
+        #chart_panel(),  
+        html.Div(id='graphs'),
+        html.Div(id='intermediate-value', style={'display': 'none'}),
+    ],
 )
 
+@dash_app.callback(Output('intermediate-value', 'children'), 
+                            [Input('chart_selector', 'value'),
+                             Input('scenario_selector', 'value')])
+def save_data(selected_charts: List, selected_scenario: int):
+     s_c = selected_charts
+     s_s = selected_scenario
+     s_data = {'sel_chrts': [s_c], 'sel_scenario':[s_s]} 
+     state_data = pd.DataFrame(data=s_data)
+     return state_data.to_json(date_format='iso', orient='split')
 
-@dash_app.callback(Output('output', 'children'), [Input('chart_selector', 'value'),
-                                                  Input('scenario_selector', 'value')])
-def display_graphs(selected_charts: List, selected_scenario: int) -> List:
-    plot_dict = plot_dicts[selected_scenario]
-    graphs = []
-    if selected_charts is not None:
-        for value in selected_charts:
-            curr_plt = plot_dict[value]
+@dash_app.callback(Output('graphs', 'children'), 
+                        [Input('intermediate-value', 'children')])
+def graphs_selected(jsonified_data):
+    charts_to_show = get_charts(jsonified_data)
+    scenario_to_show = get_scenario(jsonified_data)
+    if isinstance(charts_to_show, Iterable): 
+        return html.Div([graph(chart, scenario_to_show) for chart in charts_to_show])
+    return 
 
-            graphs.append(dcc.Graph(id=value+'-fig')) #figure=plot_dict[value]))
-            graphs.append(dcc.RangeSlider(
-                                id=value+'-slider',
-                                min=0,
-                                max=len(curr_plt['mid_plt_x']),
-                                step=None,
-                                marks={
-                                    0: '0 days',
-                                    7: '1 week',
-                                    14: '2 weeks',
-                                    40: '40 days'
-                                },
-                                value=[0, 7]
-                            )
-                        )
-        return graphs
+def graph(ch, sc):
+    plot_dict = plot_dicts[sc]
+    curr_plt = plot_dict[ch]
+
+    return html.Div(id=ch, children=[
+        dcc.Graph(id=ch+'-fig', figure=update_graph(curr_plt, [0,7])),
+        dcc.RangeSlider(id=ch+'-slider',
+                        min=0,
+                        max=len(curr_plt['mid_plt_x']),
+                        step=None,
+                        marks={
+                            0: '0 days',
+                            7: '1 week',
+                            14: '2 weeks',
+                            40: '40 days'
+                        },
+                        value=[0, 7]
+                    ),])
+
+# def display_graphs(jsonified_data) -> List:
+
+#     dff = pd.read_json(jsonified_data, orient='split')
+#     selected_charts = dff['sel_chrts'][0]
+#     selected_scenario = dff['sel_scenario'][0]
+
+#     plot_dict = plot_dicts[selected_scenario]
+#     graphs = []
+#     #if selectec_charts is not None:
+#     if isinstance(selected_charts, Iterable): 
+#         for value in selected_charts:
+#             print("adding" + value)
+#             curr_plt = plot_dict[value]
+
+#             graphs.append(dcc.Graph(id=value+'-fig')) #figure=plot_dict[value]))
+#             graphs.append(dcc.RangeSlider(
+#                                 id=value+'-slider',
+#                                 min=0,
+#                                 max=len(curr_plt['mid_plt_x']),
+#                                 step=None,
+#                                 marks={
+#                                     0: '0 days',
+#                                     7: '1 week',
+#                                     14: '2 weeks',
+#                                     40: '40 days'
+#                                 },
+#                                 value=[0, 7]
+#                             )
+#                         )
+#         return graphs
 
 @dash_app.callback(Output('Patients Presenting at Hospital-fig', 'figure'), 
                     [Input('Patients Presenting at Hospital-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_presenting(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_presenting_at_hospital(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Presenting at Hospital']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients in General Ward-fig', 'figure'), 
                     [Input('Patients in General Ward-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_gen(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_in_general_ward(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients in General Ward']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients in ICU (but not on Ventilator)-fig', 'figure'), 
                     [Input('Patients in ICU (but not on Ventilator)-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_no_vent_icu(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_in_icu_no_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients in ICU (but not on Ventilator)']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients on Ventilator in ICU-fig', 'figure'), 
                     [Input('Patients on Ventilator in ICU-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_vent_icu(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_in_icu_on_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients on Ventilator in ICU']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients that Die Each Day-fig', 'figure'), 
                     [Input('Patients that Die Each Day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_die(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_die(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients that Die Each Day']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients Added to General Ward each day-fig', 'figure'), 
                     [Input('Patients Added to General Ward each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_added_gen(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_added_general_ward(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Added to General Ward each day']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients Added to ICU (but not on vent) each day-fig', 'figure'), 
                     [Input('Patients Added to ICU (but not on vent) each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_added_no_vent_icu(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_added_icu_no_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Added to ICU (but not on vent) each day']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients Added on Ventilator in ICU each day-fig', 'figure'), 
                     [Input('Patients Added on Ventilator in ICU each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_added_vent_icu(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_added_icu_on_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Added on Ventilator in ICU each day']
     return update_graph(p_df, selected_range)
  
 @dash_app.callback(Output('Patients Discharged from General Ward each day-fig', 'figure'), 
                     [Input('Patients Discharged from General Ward each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_discharged_gen(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_discharged_general_ward(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Discharged from General Ward each day']
     return update_graph(p_df, selected_range)
  
 @dash_app.callback(Output('Patients Discharged from ICU (but not on vent) each day-fig', 'figure'), 
                     [Input('Patients Discharged from ICU (but not on vent) each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_discharged_no_vent_icu(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_discharged_icu_no_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Discharged from ICU (but not on vent) each day']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients Discharged from On Ventilator In ICU each day-fig', 'figure'), 
-                    [Input('Patients Discharged from On Ventilator In ICU each dayl-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_discharged_vent_icu(selected_range, selected_scenario):
+                    [Input('Patients Discharged from On Ventilator In ICU each day-slider', 'value'),
+                     Input('intermediate-value', 'children')])
+def update_patients_discharged_icu_on_vent(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients Discharged from On Ventilator In ICU each day']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients discharged from Presenting each day (fully recovered)-fig', 'figure'), 
                     [Input('Patients discharged from Presenting each day (fully recovered)-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_discharged_presenting(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_discharged_presenting(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients discharged from Presenting each day (fully recovered)']
     return update_graph(p_df, selected_range)
 
 @dash_app.callback(Output('Patients added to Presenting each day-fig', 'figure'), 
                     [Input('Patients added to Presenting each day-slider', 'value'),
-                     Input('scenario_selector', 'value')])
-def update_patients_added_presenting(selected_range, selected_scenario):
+                     Input('intermediate-value', 'children')])
+def update_patients_added_presenting(selected_range, jsonified_data):
+    selected_scenario = get_scenario(jsonified_data)
     p_df = plot_dicts[selected_scenario]['Patients added to Presenting each day']
     return update_graph(p_df, selected_range)
 
@@ -195,6 +255,16 @@ def update_graph(plot_df, sel_range):
             yaxis_linecolor=plot_df['plt_yaxis_linecolor'],
         )
     }
+
+def get_scenario(j_data):
+    dff = pd.read_json(j_data, orient='split')
+    selected_scenario = dff['sel_scenario'][0]
+    return selected_scenario
+
+def get_charts(j_data):
+    dff = pd.read_json(j_data, orient='split')
+    selected_charts = dff['sel_chrts'][0] 
+    return selected_charts
 
 
 if __name__ == '__main__':
